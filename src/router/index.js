6e9662d077dbfcdb61a0d5297654087ec1550d90
Tan Kwang How <tankwanghow@gmail.com>
2019-12-03 01:22:59 +0800

    added route guard

--- src/router/index.js
+++ src/router/index.js
@@ -10,17 +10,35 @@ Vue.use(VueRouter)
  * directly export the Router instantiation
  */
 
-export default function (/* { store, ssrContext } */) {
+export default function ( { store } ) {
   const Router = new VueRouter({
     scrollBehavior: () => ({ x: 0, y: 0 }),
     routes,
-
+  
     // Leave these as they are and change in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
     mode: process.env.VUE_ROUTER_MODE,
     base: process.env.VUE_ROUTER_BASE
   })
+
+  Router.beforeEach((to, from, next) => {
+    if(to.meta.requireLogin) {
+      if(!store.state.auth.currentUser.userid) {
+        next('/login')
+      }
+      else {
+        next()
+      }
+    }
+    else if ((to.name == 'login' || to.name == 'signup') && store.state.auth.currentUser.userid) {
+      next(from.fullPath)
+    }
+    else {
+      next()
+    }
+  })
+  
 
   return Router
 }
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,11 +4,32 @@ const routes = [
     path: '/',
     component: () => import('layouts/Layout.vue'),
     children: [
-      { path: '', name: 'welcome', component: () => import('pages/PageWelcome.vue') },
-      { path: '/dashboard', name: 'dashboard', component: () => import('pages/PageDashBoard.vue') },
-      { path: '/sowcard', component: () => import('pages/PageSowCard.vue') },
-      { path: '/login', name: 'login', component: () => import('pages/PageLogin.vue') },
-      { path: '/signup', name: 'signup', component: () => import('pages/PageSignup.vue') }
+      {
+        path: '',
+        name: 'welcome',
+        component: () => import('pages/PageWelcome.vue')
+      },
+      {
+        path: '/dashboard',
+        meta: { requireLogin: true }, 
+        name: 'dashboard', 
+        component: () => import('pages/PageDashBoard.vue')
+      },
+      {
+        path: '/sowcard',
+        meta: { requireLogin: true }, 
+        component: () => import('pages/PageSowCard.vue')
+      },
+      {
+        path: '/login',
+        name: 'login', 
+        component: () => import('pages/PageLogin.vue')
+      },
+      {
+        path: '/signup',
+        name: 'signup', 
+        component: () => import('pages/PageSignup.vue')
+      }
     ]
   }
 ]
