46a07b8e7175ceee380efb87ed7b516e76493195
Tan Kwang How <tankwanghow@gmail.com>
2019-12-09 22:46:26 +0800

    temp code for testing

--- src/pages/PageSowCard.vue
+++ src/pages/PageSowCard.vue
@@ -1,9 +1,24 @@
 <template>
-  <h5>Sow Card</h5>
+  <div>
+    <h5>Sow Card</h5>
+    <li v-for="f in farms" :key='f.id'>
+      {{ f.name }}
+    </li>
+  </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      farms: []
+    }
+  },
+  mounted () {
+    this.$axios.get('/farms')
+        .then((res) => {
+          this.farms = res.data
+        })
+  }
 }
 </script>
