cee762475a9fa3a401f8f31cc13556d0f03116be
Tan Kwang How <tankwanghow@gmail.com>
2019-12-14 16:49:13 +0800

    faster hide notifation

--- src/boot/notify-defaults.js
+++ src/boot/notify-defaults.js
@@ -2,7 +2,7 @@ import { Notify } from 'quasar'
 
 Notify.setDefaults({
   position: 'top',
-  timeout: 3000,
+  timeout: 2000,
   actions: [{ icon: 'close', color: 'white' }],
   classes: 'piggy_notify'
 })
